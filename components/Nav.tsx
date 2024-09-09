921d5b927325027aba13cd8bf9619967f8f42c9b
wavevion <135476335+gionmud0@users.noreply.github.com>
2024-09-09 18:24:40 +0400

    Update Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -50,7 +50,7 @@ export const Nav = () => {
           <span>
             <Github className={"size-4"} />
           </span>
-          <span>Star on GitHub</span>
+          \\\ <span>Star on GitHub</span> ///
         </Button>
         <Button
           onClick={toggleDark}
